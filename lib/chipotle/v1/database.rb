941606a679fe80d4df3e292e1b44341d8aed7cbb
aarkerio <mmontoya@gmail.com>
2015-08-11 14:39:39 -0500

    Update new libs

---
+++ lib/chipotle/v1/database.rb
@@ -0,0 +1,43 @@
+module Epublishing
+  module V1
+    module Database
+        class DbConnect
+          include ActiveModel::Validations
+          include ActiveModel::Conversion
+          extend ActiveModel::Naming
+
+          def self.connect_customer(customer)
+            EstablishDatabaseConnection.new(self.new).connect(customer)
+          end
+
+          # Public: determine HTTP_HOST from params passed to rake task.
+          #
+          # Returns HTTP_HOST string.
+          def self.http_client_from_params(params)
+            return 'localhost'
+
+            client = params[:client] or fail 'Client name not provided.'
+
+            case Rails.env
+            when 'production'
+              "#{client}.enhanced-admin.epublishing.com"
+            when 'staging'
+              "#{client}.enhanced-admin-stage.epublishing.com"
+            when 'local'
+              "localhost"
+            else
+              fail "Environment is not production or staging (#{Rails.env})"
+            end
+          end
+
+        # Private: run a search and output the result.
+        #
+        # Returns nothing
+        def call(env)
+          #export_results
+          return true
+        end
+      end  # class DbConnect
+    end   # module Database
+  end  # module V1
+end # module Epublishing
