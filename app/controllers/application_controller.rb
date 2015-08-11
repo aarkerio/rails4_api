4b94630171d0df950d482e6ec5e626809dc8a5c3
aarkerio <mmontoya@gmail.com>
2015-08-11 12:26:38 -0500

    new libs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,23 +14,6 @@ class ApplicationController < ActionController::Base
   end
 
   def authentication_check
-
-    if Rails.env != 'test'
-      db = { adapter: 'mysql2', username: 'api_epub', password: 'c4SExA8J' }
-      case Rails.env
-      when 'local'
-        db[:database] = 'api_epub_local'
-        db[:host]     = 'localhost'
-      when 'staging'
-        db[:database] = 'api_epub_staging'
-        db[:host]     = 'rails-stage-app-1.cloud.epub.net'
-      when 'production'
-        db[:database] = 'api_epub_production'
-        db[:host]     = 'rails-app-1.cloud.epub.net'
-      end
-      ActiveRecord::Base.establish_connection(db)
-    end
-
     authenticate_or_request_with_http_basic do |user, password|
       hashed_pwd = Digest::SHA256.hexdigest(password)
       @customer   = Customer.where(api_key: user, secret_key: hashed_pwd).first
